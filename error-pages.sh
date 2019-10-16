f1613e9523fff076dfe4d1c6f04b9a51d8761f87
Andrew Johnston <asjohnston@alaska.edu>
2019-10-16 07:07:46 -0800

    update error page list

--- error-pages.sh
+++ error-pages.sh
@@ -1,14 +1,7 @@
 HOST=$1
 PREFIX=${HOST}/error_doc
 
-/opt/google/chrome/chrome --incognito ${PREFIX}/404.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/409.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/500.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/502.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/503.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/unrestricted/401.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/restricted/401.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/sentinel/401.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/sentinel/403.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/palsar/401.html
-/opt/google/chrome/chrome --incognito ${PREFIX}/palsar/403.html
+/opt/google/chrome/chrome --incognito ${PREFIX}/file_not_found.html
+/opt/google/chrome/chrome --incognito ${PREFIX}/data_temporarily_unavailable.html
+/opt/google/chrome/chrome --incognito ${PREFIX}/unauthorized.html
+/opt/google/chrome/chrome --incognito ${PREFIX}/internal_server_error.html
